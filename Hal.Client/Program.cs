dd44595c654b01305be9e81385c113f39f390e0f
Xintia Gyenge <xigyenge@microsoft.com>
2017-10-09 13:40:13 +0300

    First commit

---
+++ Hal.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hal.Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new HttpClient();
+			var response = client.GetAsync("http://datc-rest.azurewebsites.net/beers").Result;
+
+			var data = response.Content.ReadAsStringAsync().Result;
+			var obj = JsonConvert.DeserializeObject(data);
+
+			Console.WriteLine(data);
+
+			Console.ReadLine();
+		}
+	}
+}
